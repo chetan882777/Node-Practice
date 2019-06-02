ac31ab7a198c021b9c371f4d8b4e23a4c99bc379
chetan882777 <chetan882777@gmail.com>
2019-06-02 16:04:19 +0530

    play with json

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJson = dataBuffer.toString()
+const user = JSON.parse(dataJson)
+
+user.name = "Andrew"
+user.age = 27
+
+const userJson = JSON.stringify(user)
+
+fs.writeFileSync('1-json.json', userJson)
