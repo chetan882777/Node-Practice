b10b25fc29addde1e51a56d3e0363ea7d090103d
chetan882777 <chetan882777@gmail.com>
2019-06-02 15:20:59 +0530

    yargs module

--- MyFirstScript.js
+++ MyFirstScript.js
@@ -1,8 +1,45 @@
 const chalk = require("chalk")
 const sum = require("./utils")
+const yargs = require("yargs")
 
 const result = sum(1, 3)
 
 console.log(result)
 
-console.log(chalk.inverse.green("success"))
+console.log(chalk.red("success"))
+
+yargs.version("1.1.0")
+
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    handler: function(){
+        console.log("Adding a new note!")
+    } 
+})
+
+yargs.command({
+    command: "remove",
+    describe: "Romove a note",
+    handler: function(){
+        console.log("Removing a note!")
+    } 
+})
+
+yargs.command({
+    command: "list",
+    describe: "List the notes",
+    handler: function(){
+        console.log("Listing note!")
+    } 
+})
+
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function(){
+        console.log("Reading note!")
+    } 
+})
+
+console.log(yargs.argv)
